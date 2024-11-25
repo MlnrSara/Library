7bcb9b8579ba526bf99a8b3fb3f3b371b73bf74d
Molnar Sara <saraviviana68@gmail.com>
2024-11-25 19:03:18 +0200

    added Order model, repository and table

---
+++ src/main/java/model/Order.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Order {
+    private Long id;
+    private LocalDateTime timestamp;
+    private Long employeeId;
+    private String title;
+    private String author;
+    private Float price;
+    private Integer stock;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+}
---
+++ src/main/java/model/builder/OrderBuilder.java
@@ -0,0 +1,48 @@
+package model.builder;
+
+import model.Order;
+
+import java.time.LocalDateTime;
+
+public class OrderBuilder {
+    private Order order;
+
+    public OrderBuilder setId(Long id){
+        order.setId(id);
+        return this;
+    }
+
+    public OrderBuilder setTimestamp(LocalDateTime timestamp){
+        order.setTimestamp(timestamp);
+        return this;
+    }
+
+    public OrderBuilder setEmployeeId(Long id){
+        order.setEmployeeId(id);
+        return this;
+    }
+
+    public OrderBuilder setTitle(String title){
+        order.setTitle(title);
+        return this;
+    }
+
+    public OrderBuilder setAuthor(String author){
+        order.setAuthor(author);
+        return this;
+    }
+
+    public OrderBuilder setPrice(Float price){
+        order.setPrice(price);
+        return this;
+    }
+
+    public OrderBuilder setStock(Integer stock){
+        order.setStock(stock);
+        return this;
+    }
+
+    public Order build(){
+        return order;
+    }
+}
